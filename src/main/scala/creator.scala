f4f17b84412fb43a5700c830b7200d522dd08397
Viktor Tusa <tusavik@gmail.com>
2017-05-19 18:42:13 +0200

    API now supports setting parser type.

--- src/main/scala/creator.scala
+++ src/main/scala/creator.scala
@@ -48,7 +48,11 @@ package hyperion {
         case "printer" => system.actorOf(Props(new Printer(id)), nameFromId(id))
         case "source" => {
           val port: Int = options.options("port").toInt
-          val parser = system.actorOf(Props(new TcpSource(id, port, "syslog")), nameFromId(id))
+          val parser = options.options get "parser" match {
+            case Some(result) => result
+            case None => "syslog"
+          }
+          val tcpsource = system.actorOf(Props(new TcpSource(id, port, parser)), nameFromId(id))
 
         }
         case "counter" => system.actorOf(Props(new MessageCounter(id)), nameFromId(id))
