654986a97f4531138447adc7655866aac4b11270
Viktor Tusa <tusavik@gmail.com>
2017-07-03 22:05:21 +0200

    Flush messages in ElasticSearch after 1 sec.

--- src/main/scala/pipes/elasticsearch.scala
+++ src/main/scala/pipes/elasticsearch.scala
@@ -3,6 +3,7 @@ import scala.concurrent.duration.{FiniteDuration, SECONDS}
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
 import com.sksamuel.elastic4s.http.ElasticDsl._
+import com.github.nscala_time.time.Imports.DateTime
 
 package hyperion {
 
@@ -10,10 +11,17 @@ package hyperion {
     def selfId = id
     val client = HttpClient(ElasticsearchClientUri(host, port))
     var messages : ListBuffer[Message] = ListBuffer[Message]();
+    var lastMessage = DateTime.now
 
     override def preStart() {
       super.preStart();
       startTicking(FiniteDuration(1, SECONDS),FiniteDuration(1, SECONDS))
+    }
+
+    def flushMessages() = {
+      val command = bulk(messages.toSeq.map(indexInto("logstash-2017-06-26/log") fields _.nvpairs ))
+      client.execute(command).await
+      messages.clear()
     }
 
     def process = {
@@ -21,9 +29,15 @@ package hyperion {
         //val command = indexInto("logstash-2017-06-26/log").fields(msg.nvpairs)
         messages.append(msg);
         if (messages.length >= 100) {
-          var command = bulk(messages.toSeq.map(indexInto("logstash-2017-06-26/log") fields _.nvpairs ))
-          client.execute(command).await
-          messages.clear()
+          flushMessages()
+        }
+
+      }
+
+      case Tick => {
+
+        if (DateTime.now.minus(lastMessage.getMillis).getMillis > 1000L) {
+          flushMessages()
         }
 
       }
